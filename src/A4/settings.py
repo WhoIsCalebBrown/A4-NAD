1792d28008da2b353460d1d9595c0dc737e04113
Caleb <cbrown4154@conestogac.on.ca>
2023-03-21 17:52:52 -0400

    idk im just following the tut

--- src/A4/settings.py
+++ src/A4/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIR = [
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
     BASE_DIR  / 'static',
     BASE_DIR / 'posts' / 'static',
     BASE_DIR / 'profiles' / 'static'
